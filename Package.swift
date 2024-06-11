207e135f7533fd4570644b755f8ac58d7a7e42bf
mesqueeb <mesqueeb@users.noreply.github.com>
2024-06-11 18:18:16 +0900

    chore: 🎉 Release version 0.3.1

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "FuegoXCFramework",
-      url: "https://github.com/mesqueeb/FuegoOnAppleSilicon/releases/download/v0.3.0/Fuego-0.3.0.xcframework.zip",
+      url: "https://github.com/mesqueeb/FuegoOnAppleSilicon/releases/download/v0.3.1/Fuego-0.3.1.xcframework.zip",
       checksum: "94eda59dc7c819c4a9a69618d62232da410bda9b4576085ed25bce7bdc26bf2b"
     ),
     .target(
